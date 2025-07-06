5f51565f6c66e9d56ecc2b395ba36f33a16e412f
sblecksmith <99356245+sblecksmith@users.noreply.github.com>
2025-07-05 18:22:04 -0700

    Figure 4 is now the muc2plant plots

--- scripts/figure_4.R
+++ scripts/figure_4.R
@@ -1,12 +1,14 @@
 # title: figure_4
 # author: Sarah Blecksmith
-# purpose:  Make figure 5, plant cazyme Shannon diversity and abundance with fecal calprotectin and fecal mpo
+# purpose:  Make figure 4, muc2plant plots
 
-
+library(dplyr)
 library(ggplot2)
 library(ggpmisc)
+library(ggpubr)
 library(gridExtra)
 library(showtext)
+library(bestNormalize)
 
 font_add_google("Montserrat", "mont")
 showtext_auto()
@@ -15,21 +17,33 @@ showtext_auto()
 FL100_data <- read.csv("data/FL100_merged_variables.csv", header = TRUE) 
 microbiome_data <- read.csv("data/microbiome_merged_variablesGH_GHPL.csv", header = TRUE) 
 
+
+# calculate number of subjects above 1 standard deviation for muc2plant
+sum(microbiome_data$muc2plantGHPL - mean(microbiome_data$muc2plantGHPL) >= sd(microbiome_data$muc2plantGHPL)) #34
+
+# calculate number of subjects below 1 standard deviation for muc2plant
+sum(microbiome_data$muc2plantGHPL - mean(microbiome_data$muc2plantGHPL) <= -sd(microbiome_data$muc2plantGHPL)) #32
+
+
 # merge the data
 merged = FL100_data %>%
   right_join(microbiome_data, by = "subject_id") %>%
   mutate(sex = factor(sex))
 
 
-inflamm_variables = c("fecal_neopterin", "fecal_mpo")
+inflamm_variables = c("fecal_calprotectin", "fecal_neopterin")
 
 # read in saved transformations
 inflammation_transformations <- read.csv('data/inflammation_transformations.csv', header = TRUE)
+microbiome_transformations <- read.csv("data/microbiome_transformations.csv", header = TRUE) 
+
+# merge the transformations
+transformations <- rbind(inflammation_transformations,microbiome_transformations)
 
 # Transform the variables
-for (outcome in inflammation_transformations$Outcome) {
+for (outcome in transformations$Outcome) {
   #Extract recommended transformation for outcome variable   
-  transform_recommendation = (inflammation_transformations %>% filter(Outcome == outcome))[1,2]
+  transform_recommendation = (transformations %>% filter(Outcome == outcome))[1,2]
   
   #Retrieve the function object
   transform = match.fun(transform_recommendation)
@@ -40,81 +54,64 @@ for (outcome in inflammation_transformations$Outcome) {
   merged[,outcome_transformed] = transform(merged[[outcome]])$x.t
 }
 
-# NEOPTERIN
-merged_neopt <- subset(merged, !is.na(merged[,"fecal_neopterin"]))
-# Neopterin - Plant cazyme Shannon
-shannon_neopt_resid <- resid(lm(data=merged_neopt, Shannon ~ age + sex + bmi_final))
-neopt_resid <- resid(lm(data=merged_neopt,fecal_neopterin_yeojohnson ~ age + sex + bmi_final))
-
-shannon_neopt_resid <- as.numeric(shannon_neopt_resid)
-neopt_resid <- as.numeric(neopt_resid)
 
 
-p1 <- ggplot(data = NULL, aes(x=shannon_neopt_resid, y=neopt_resid)) +
-  geom_point(aes(colour = "#464646"))+
-  theme(text = element_text(size=12,family = "mont"),
-        axis.title.y = element_text(margin=margin(r=15)),
-        axis.title.x = element_text(margin=margin(t=15))) +
-  scale_color_identity() +
-  #scale_color_manual(values = "#464646") +
-  geom_smooth(method = "lm", formula = y~x, se = F, color = "#f14902") +
-  stat_poly_eq(
-    geom = "text",
-    hjust = 0,
-    aes(label = paste(after_stat(p.value.label),
-                      after_stat(n.label),
-                      after_stat(rr.label), sep = "*\", \"*")),
-    size = 5) +
-  labs(title = "Plant CAZyme Shannon diversity by normalized fecal neopterin",
-       subtitle = "Covariates age, sex and BMI",
-       x = "Plant CAZyme Shannon Diversity",
-       y = "Normalized Fecal Neopterin",
+p1 <- ggplot(merged, aes(x=muc2plantGHPL)) +
+  theme(text = element_text(size=14,family = "mont")) +
+  geom_density(fill="#47818d", color="#e9ecef", alpha=0.8) +
+  labs(x= "Muc2plant", y="Density",
        tag = "A")
 
-# Neopterin - Plant cazyme abundance
-abund_neopt_resid <- resid(lm(data=merged_neopt, plant_cazyme_abundance ~ age + sex + bmi_final))
-neopt_resid <- resid(lm(data=merged_neopt,fecal_neopterin_yeojohnson ~ age + sex + bmi_final))
-
-abund_neopt_resid <- as.numeric(abund_neopt_resid)
-neopt_resid <- as.numeric(neopt_resid)
-
-
-p2 <- ggplot(data = NULL, aes(x=abund_neopt_resid, y=neopt_resid)) +
+p2 <- ggplot(merged,aes(y=muc2plantGHPL_log_x,x=bmi_final))+
   geom_point(aes(colour = "#464646"))+
-  theme(text = element_text(size=12,family = "mont"),
-        axis.title.y = element_text(margin=margin(r=15)),
-        axis.title.x = element_text(margin=margin(t=15))) +
+  theme(text = element_text(size=14,family = "mont"),
+        axis.title.y = element_text(margin=margin(r=13)),
+        axis.title.x = element_text(margin=margin(t=15)))+
+  #scale_y_continuous(expansion(c(0,0.08)))+
   scale_color_identity() +
-  #scale_color_manual(values = "#464646") +
+  xlim(NA,43.9) +
   geom_smooth(method = "lm", formula = y~x, se = F, color = "#f14902") +
   stat_poly_eq(
     geom = "text",
-    hjust = 0,
+    label.y = 4,
+    label.x = 30,
     aes(label = paste(after_stat(p.value.label),
                       after_stat(n.label),
                       after_stat(rr.label), sep = "*\", \"*")),
-    size = 5) +
-  labs(title = "Plant CAZyme abundance by normalized fecal neopterin",
-       subtitle = "Covariates age, sex and BMI",
-       x = "Plant CAZyme Abundance",
-       y = "Normalized Fecal Neopterin",
+    size = 4.5) +
+  labs(y = "Muc2plant", 
+       x = "BMI",
        tag = "B") 
 
+p3 <- merged %>%
+  mutate(sex = ifelse(sex == 1, "Male", "Female")) %>%
+  ggplot(aes(x = sex, y = muc2plantGHPL, fill = sex))+
+  geom_boxplot()+ 
+  theme(text = element_text(family = "mont", size=12),
+        axis.title.y = element_text(margin=margin(r=12)),
+        axis.title.x = element_text(margin=margin(t=15)),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  scale_fill_manual(values = c("#47818d","#f14902"), guide = "none") +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()) +
+
+  labs(
+       y = "Muc2plant",
+       x = "sex",
+       tag = "C") +
+  stat_compare_means(method = "wilcox.test", label.x = 1, label.y = .077, size=4.5, vjust = .99)
 
 
+#Plotting residuals
+ratio_resid = resid(lm(data = merged, muc2plantGHPL ~ age + sex + bmi_final))
+cal_resid = resid(lm(data = merged, fecal_calprotectin_yeojohnson ~ age + sex + bmi_final))
 
 
-# MPO
-# MPO - Plant cazyme Shannon
-shannon_resid <- resid(lm(data=merged, Shannon ~ age + sex + bmi_final))
-mpo_resid <- resid(lm(data=merged,fecal_mpo_boxcox ~ age + sex + bmi_final))
-
-shannon_resid <- as.numeric(shannon_resid)
-mpo_resid <- as.numeric(mpo_resid)
-
-p3 <-ggplot(data = NULL, aes(x=shannon_resid, y=mpo_resid)) +
+p4 <- ggplot(data = NULL, aes(x=ratio_resid,y=cal_resid))+
   geom_point(aes(colour = "#464646"))+
-  theme(text = element_text(size=12,family = "mont"),
+  theme(text = element_text(size=14,family = "mont"),
         axis.title.y = element_text(margin=margin(r=15)),
         axis.title.x = element_text(margin=margin(t=15))) +
   scale_color_identity() +
@@ -122,29 +119,28 @@ p3 <-ggplot(data = NULL, aes(x=shannon_resid, y=mpo_resid)) +
   geom_smooth(method = "lm", formula = y~x, se = F, color = "#f14902") +
   stat_poly_eq(
     geom = "text",
-    hjust = 0,
+    label.x = -0.005,
+    label.y = 4.4,
     aes(label = paste(after_stat(p.value.label),
                       after_stat(n.label),
                       after_stat(rr.label), sep = "*\", \"*")),
     size = 5) +
-  labs(title = "Plant CAZyme Shannon diversity by normalized fecal myeloperoxidase",
-       subtitle = "Covariates age, sex and BMI",
-       x = "Plant CAZyme Shannon Diversity",
-       y = "Normalized Fecal Myeloperoxidase",
-       tag = "C") 
+  labs(
+       tag = "D",
+       x = "Muc2plant",
+       y = "Calprotectin") 
+
+p4
+
+# There aren't neopterin measures for everyone
+merged_neopt <- subset(merged, !is.na(merged[,"fecal_neopterin"]))
+ratio_neopt_resid = resid(lm(data = merged_neopt, muc2plantGHPL ~ age + sex + bmi_final))
+neopt_resid = resid(lm(data = merged_neopt, fecal_neopterin_yeojohnson ~ age + sex + bmi_final))
 
 
-# MPO - Plant cazyme abundance
-abund_resid <- resid(lm(data=merged, plant_cazyme_abundance ~ age + sex + bmi_final))
-mpo_resid <- resid(lm(data=merged,fecal_mpo_boxcox ~ age + sex + bmi_final))
-
-abund_resid <- as.numeric(abund_resid)
-mpo_resid <- as.numeric(mpo_resid)
-
-
-p4 <- ggplot(data = NULL, aes(x=abund_resid, y=mpo_resid)) +
+p5 <- ggplot(data = NULL, aes(x=ratio_neopt_resid,y=neopt_resid))+
   geom_point(aes(colour = "#464646"))+
-  theme(text = element_text(size=12,family = "mont"),
+  theme(text = element_text(size=14,family = "mont"),
         axis.title.y = element_text(margin=margin(r=15)),
         axis.title.x = element_text(margin=margin(t=15))) +
   scale_color_identity() +
@@ -152,18 +148,26 @@ p4 <- ggplot(data = NULL, aes(x=abund_resid, y=mpo_resid)) +
   geom_smooth(method = "lm", formula = y~x, se = F, color = "#f14902") +
   stat_poly_eq(
     geom = "text",
-    hjust = 0,
+    label.y = 2.6,
+    label.x = -.005,
     aes(label = paste(after_stat(p.value.label),
                       after_stat(n.label),
                       after_stat(rr.label), sep = "*\", \"*")),
     size = 5) +
-  labs(title = "Plant CAZyme abundance by normalized fecal myeloperoxidase",
-       subtitle = "Covariates age, sex and BMI",
-       x = "Plant CAZyme Abundance",
-       y = "Normalized Fecal Myeloperoxidase",
-       tag = "D")
+  labs(
+       x = "Muc2plant",
+       y = "Neopterin",
+       tag = "E") 
+
+p5
+
+fig4 <- grid.arrange(p1, p2, p3,p4,p5, clip="off",
+                 #widths=c(1,1,1),
+                 layout_matrix = rbind(c(1,1,2,3),
+                                       c(1,1,2,3),
+                                       c(4,4,5,5),
+                                       c(4,4,5,5)))
+
+ggsave("figure_4.tiff", device = "tiff", dpi = 300, width = 14, height = 8, units = "in", path = "output", fig4)
 
 
-fig4 <- grid.arrange(p1, p2, p3, p4, ncol=2)
-
-ggsave("figure_4.tiff", device = "tiff", dpi = 300, width = 15, height = 10, units = "in", path = "output", fig4)
